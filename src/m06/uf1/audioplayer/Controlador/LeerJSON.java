04d63d6cbe5087824c3e21809164f153134c89b0
khugar <ksangon98@gmail.com>
2019-05-03 12:55:55 +0200

    LeerJSON WIP

--- src/m06/uf1/audioplayer/Controlador/LeerJSON.java
+++ src/m06/uf1/audioplayer/Controlador/LeerJSON.java
@@ -1,5 +1,32 @@
 package m06.uf1.audioplayer.Controlador;
 
+import java.io.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Kevin
+ */
 public class LeerJSON {
-    
+
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject playlist = (JSONObject) parser.parse(new FileReader("playlist.json"));
+            System.out.println(playlist.get("especie"));
+            JSONArray races = (JSONArray) playlist.get("races");
+            for (Object r : races) {
+                System.out.println(r);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
