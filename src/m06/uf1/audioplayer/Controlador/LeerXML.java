13ead58988fadaf8a8c6a7617bb1d04954937d26
Rafa <gonzalezrafata@gmail.com>
2019-05-03 14:29:18 +0200

    Clases lista, modificaciones XML, nuevo metodo para obtener listas, jar de Json implementado, etc.

--- src/m06/uf1/audioplayer/Controlador/LeerXML.java
+++ src/m06/uf1/audioplayer/Controlador/LeerXML.java
@@ -16,6 +16,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import m06.uf1.audioplayer.Modelo.Audio;
+import m06.uf1.audioplayer.Modelo.Lista;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -24,24 +25,14 @@ import org.xml.sax.SAXException;
 
 public class LeerXML {
 
-    public Map<Integer, Audio> getAudios() {
-        return audios;
-    }
-    
     private Map<Integer, Audio> audios;
+    private Map<Integer, Lista> listas;
     private static LeerXML lector;
     private String ubi = "audios/lista.xml";
-        
-    public static LeerXML getLectorInstance(){
-        if(lector==null){
-            lector = new LeerXML();
-        }
-        return lector;
-    }
-    
-    private LeerXML(){
+
+    private LeerXML() {
         try {
-            carga(ubi);
+            carga();
         } catch (IOException ex) {
             Logger.getLogger(LeerXML.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParserConfigurationException ex) {
@@ -49,52 +40,104 @@ public class LeerXML {
         }
     }
 
-    private void carga(String nomArxiu) throws IOException, ParserConfigurationException {
+    private void carga() throws IOException, ParserConfigurationException {
         audios = new HashMap();
-        carregarAudios(nomArxiu);
+        carregarAudios();
+
+        listas = new HashMap();
+        carregarListas();
     }
 
-    public void carregarAudios(String arxiuAudios) throws FileNotFoundException, IOException, ParserConfigurationException {
+    private void carregarAudios() throws FileNotFoundException, IOException, ParserConfigurationException {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         Document document;
         DocumentBuilder builder = factory.newDocumentBuilder();
 
         try {
-            File f = new File(arxiuAudios);
-            if(f.exists()){
-            System.out.println(f.exists());
-            document = builder.parse(f);
-            Element arrel = document.getDocumentElement();
-            NodeList llistaAudios = arrel.getChildNodes();
-            for (int i = 0; i < llistaAudios.getLength(); i++) {
-                Node node = llistaAudios.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element audio = (Element) node;
-                    int id = Integer.parseInt(audio.getAttribute("id"));
-                    Element nom = (Element) audio.getElementsByTagName("nom").item(0);
-                    Element autor = (Element) audio.getElementsByTagName("autor").item(0);
-                    Element album = (Element) audio.getElementsByTagName("album").item(0);
-                    Element durada = (Element) audio.getElementsByTagName("durada").item(0);
-                    Element ruta = (Element) audio.getElementsByTagName("ruta").item(0);
+            File f = new File(ubi);
+            if (f.exists()) {
+                System.out.println(f.exists());
+                document = builder.parse(f);
+                Element arrel = document.getDocumentElement();
+                NodeList llistaAudios = arrel.getElementsByTagName("song");
+                for (int i = 0; i < llistaAudios.getLength(); i++) {
+                    Node node = llistaAudios.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element audio = (Element) node;
+                        int id = Integer.parseInt(audio.getAttribute("id"));
+                        Element nom = (Element) audio.getElementsByTagName("name").item(0);
+                        Element autor = (Element) audio.getElementsByTagName("author").item(0);
+                        Element album = (Element) audio.getElementsByTagName("album").item(0);
+                        Element durada = (Element) audio.getElementsByTagName("duration").item(0);
+                        Element ruta = (Element) audio.getElementsByTagName("route").item(0);
 
-                    Audio aud = new Audio(id,
-                            nom.getChildNodes().item(0).getNodeValue(),
-                            autor.getChildNodes().item(0).getNodeValue(),
-                            album.getChildNodes().item(0).getNodeValue(),
-                            durada.getChildNodes().item(0).getNodeValue(),
-                            ruta.getChildNodes().item(0).getNodeValue());
-                            
-                    audios.put(id, aud);
+                        Audio aud = new Audio(id,
+                                nom.getChildNodes().item(0).getNodeValue(),
+                                autor.getChildNodes().item(0).getNodeValue(),
+                                album.getChildNodes().item(0).getNodeValue(),
+                                durada.getChildNodes().item(0).getNodeValue(),
+                                ruta.getChildNodes().item(0).getNodeValue());
+
+                        audios.put(id, aud);
+                    }
                 }
+            } else {
+                System.out.println("Error! fichero no encontrado. Asegurate que el xml esta en " + ubi);
             }
-        }else{
-                System.out.println("Error! fichero no encontrado. Asegurate que el xml esta en "+ubi);
-            }
-            }catch (SAXException ex) {
+        } catch (SAXException ex) {
             ex.printStackTrace();
         }
     }
+
+    private void carregarListas() throws FileNotFoundException, IOException, ParserConfigurationException {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        Document document;
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        try {
+
+            document = builder.parse(new File(ubi));
+            Element arrel = document.getDocumentElement();
+            NodeList listaPlaylist = arrel.getElementsByTagName("playlist");
+
+            for (int i = 0; i < listaPlaylist.getLength(); i++) {
+                Node node = listaPlaylist.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element canciones = (Element) node;
+                    int id = Integer.parseInt(canciones.getAttribute("id"));
+                    Element nombre = (Element) canciones.getElementsByTagName("name").item(0);
+                    Element ruta = (Element) canciones.getElementsByTagName("route").item(0);
+
+                    Lista list = new Lista(id,
+                            nombre.getChildNodes().item(0).getNodeValue(),
+                            ruta.getChildNodes().item(0).getNodeValue());
+
+                    listas.put(id, list);
+                }
+            }
+
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Map<Integer, Audio> getAudios() {
+        return audios;
+    }
+    
+    public Map<Integer, Lista> getListas() {
+        return listas;
+    }
+
+    public static LeerXML getLectorInstance() {
+        if (lector == null) {
+            lector = new LeerXML();
+        }
+        return lector;
+    }
+
     public static void main(String[] args) {
         LeerXML lXML = LeerXML.getLectorInstance();
     }
---
+++ src/m06/uf1/audioplayer/Modelo/Lista.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m06.uf1.audioplayer.Modelo;
+
+
+public class Lista{
+    private int id;
+    private String nom,ruta;
+    
+    public Lista(int id, String nom, String ruta){
+        this.id = id;
+        this.nom = nom;
+        this.ruta = ruta;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getRuta() {
+        return ruta;
+    }
+}
