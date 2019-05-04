7cf92429bfb674466d11cdf70ab1e1e552fccab5
khugar <ksangon98@gmail.com>
2019-05-04 17:21:58 +0200

    metodo Seleccionar Playlist en leerJSON (aun en desarrollo)

--- src/Controlador/LeerJSON.java
+++ src/Controlador/LeerJSON.java
@@ -16,16 +16,13 @@ public class LeerJSON {
     public static Playlist SeleccionarPlaylist() {
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader("audios/playlistAnimo.json"));
-            JSONObject jsonObject = (JSONObject) obj;
-            String name = (String) jsonObject.get("nombre");
-            System.out.println("Nombre Playlist :  " + name);
-
-            String location = (String) jsonObject.get("descripcion");
-            System.out.println("Descripcion Playlist " + name + " :  " + location);
-            
-            JSONArray Audios = (JSONArray) jsonObject.get("audio");
-            
+            JSONObject playlist = (JSONObject) parser.parse(new FileReader("audios/playlistAnimo.json"));
+            JSONArray listaCanciones = (JSONArray) playlist.get(parser);
+            int[] canciones = new int [listaCanciones.size()];
+            for (int i = 0; i < listaCanciones.size(); i++) {
+                canciones[i] = Integer.parseInt(listaCanciones.get(i).toString()); 
+            }
+            Playlist seleccionada = new Playlist(playlist.get("nom").toString(), playlist.get("descripcion").toString(), canciones);    
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -36,6 +33,7 @@ public class LeerJSON {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     public static void maino(String[] args) {
