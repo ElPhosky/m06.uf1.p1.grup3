b001e54275dbf04b452e519ddadba9db45c91092
khugar <ksangon98@gmail.com>
2019-05-04 15:07:53 +0200

    Cambios LeerJSON

--- src/m06/uf1/audioplayer/Controlador/LeerJSON.java
+++ src/m06/uf1/audioplayer/Controlador/LeerJSON.java
@@ -15,10 +15,35 @@ public class LeerJSON {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         try {
+            Object obj = parser.parse(new FileReader("playlist.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            String name = (String) jsonObject.get("nombre");
+            System.out.println("Nombre Playlist :  " + name);
+
+            String location = (String) jsonObject.get("descripcion");
+            System.out.println("Descripcion Playlist " + name + " :  " + location);
+            
+            JSONArray Audios = (JSONArray) jsonObject.get("audio");
+            
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void maino(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
             JSONObject playlist = (JSONObject) parser.parse(new FileReader("playlist.json"));
-            System.out.println(playlist.get("especie"));
-            JSONArray races = (JSONArray) playlist.get("races");
-            for (Object r : races) {
+            System.out.println(playlist.get("nombre"));
+            JSONArray Audios = (JSONArray) playlist.get("Audio");
+            for (Object r : Audios) {
                 System.out.println(r);
             }
         } catch (FileNotFoundException e) {
