9528ade8102d169d7fc6a3b9faf53651a95d7019
khugar <ksangon98@gmail.com>
2019-05-05 18:04:08 +0200

    Inicio de la implementacion del reproductor

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -1,6 +1,7 @@
 package Controlador;
 
 import Modelo.Audio;
+import Modelo.AudioPlayer;
 import Modelo.Playlist;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,17 +13,48 @@ import Vista.VistaReproductor;
 public class Controlador implements ActionListener {
 
     private VistaReproductor vista;
-    private Audio audio;
+    private AudioPlayer audioPlayer;
     private Playlist listaActual;
 
     public Controlador() {
         vista.inicio();
         
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+     public void afegirListenerBotons() {
+        vista.getPlay().addActionListener(this);
+        vista.getStop().addActionListener(this);
+        vista.getAnterior().addActionListener(this);
+        vista.getSiguiente().addActionListener(this);
+        vista.getPausa().addActionListener(this);
+        vista.getContinuar().addActionListener(this);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent esdeveniment) {
+        //Declarem el gestor d'esdeveniments
+        Object gestorEsdeveniments = esdeveniment.getSource();
+        try {
+            if (gestorEsdeveniments.equals(vista.getPlay())) { //Si hem pitjat el boto play
+                audioPlayer.getPlayer().play(); //reproduim l'àudio
+            } else if (gestorEsdeveniments.equals(vista.getStop())) {
+                //Si hem pitjat el boto stop
+                audioPlayer.getPlayer().stop(); //parem la reproducció de l'àudio
+            } else if (gestorEsdeveniments.equals(vista.getAnterior())) {
+                //Si hem pitjat el boto stop
+                audioPlayer.getPlayer().pause(); //pausem la reproducció de l'àudio
+            } else if (gestorEsdeveniments.equals(vista.getSiguiente())) {
+                //Si hem pitjat el boto stop
+                audioPlayer.getPlayer().resume(); //continuem la reproducció de l'àudio
+            }else if (gestorEsdeveniments.equals(vista.getContinuar())) {
+                //Si hem pitjat el boto stop
+                audioPlayer.getPlayer().resume(); //continuem la reproducció de l'àudio
+            }else if (gestorEsdeveniments.equals(vista.getPausa())) {
+                //Si hem pitjat el boto stop
+                audioPlayer.getPlayer().pause(); //continuem la reproducció de l'àudio
+            }
+        } catch (BasicPlayerException e) {
+            e.printStackTrace();
+        }
+    }
 }
